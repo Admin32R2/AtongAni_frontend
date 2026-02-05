7b35a16343959fa9e23dfdc2350a3314666d8b93
Ralph <ralphtajantajan4@gmail.com>
2026-02-05 11:45:19 +0800

    update in reload pages

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Pages from 'vite-plugin-pages'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), Pages()],
   base: '/AtongAni_frontend/'
 })
